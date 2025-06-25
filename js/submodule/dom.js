9eb265dfc0d65ab77bc535267e2ca3497f91c33a
Kanggwanju <xprtktm1@gmail.com>
2025-06-25 14:10:00 +0900

    feat: 낚시 게임 모달 UI 구성을 위한 DOM 요소 추가

---
+++ js/submodule/dom.js
@@ -0,0 +1,16 @@
+// ======= DOM 가져오기 ======== //
+const $gaugeBar = document.getElementById('gauge-bar');
+const $message = document.getElementById('message');
+const $clickBtn = document.getElementById('modal-click-btn');
+const $scoreDisplay = document.getElementById('score-display');
+const $modalGameContents = document.getElementById('modal-content');
+const $modalWatch = document.getElementById('modal-watch');
+
+export default {
+  $gaugeBar,
+  $message,
+  $clickBtn,
+  $scoreDisplay,
+  $modalGameContents,
+  $modalWatch,
+};
